1ea22303d5c13566f33bbb02cf4ea2b33c5d05dd
Dalitso Sakala <dalitso.1sc@gmail.com>
2023-07-06 08:31:31 +0200

    Correctly implemented environment detection

--- src/index.ts
+++ src/index.ts
@@ -80,7 +80,26 @@
         }
     }
 
-    let isBrowser=new Function("try {return this===window;}catch(e){ return false;}");
+    function isBrowser() {
+        var process:any=process||null
+        var require:any=require||null
+        // Check if the environment is Node.js
+        if (typeof process === "object" &&
+            typeof require === "function") {
+            return false;
+        }
+
+        // Check if the environment is a
+        // Service worker
+        if (typeof importScripts === "function") {
+            return false;
+        }
+
+        // Check if the environment is a Browser
+        if (typeof window === "object") {
+            return true;
+        }
+    }
     const WEEK_PACK = { prependWeeks, appendWeeks, generateInitialWeeks }
     if (isBrowser())
         Object.assign(global, { WEEK_PACK })
